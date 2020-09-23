a0890b34a41c78c2ea78188fa69c08fec0e77098
sushkr34 <sendmailtosushant@gmail.com>
2020-09-23 17:07:19 +0530

    dyanmic image

--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,24 @@
 import React,{useState} from 'react';
 import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from 'react-native'
+import Card from '../shared/card';
 import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
     const [reviews,setReviews]= useState([
-        {title:'Data Structure and algorithm',rating:5,body:'dsa by java',key:'1'},
-        {title:'React.Js and redux ',rating:8,body:'Html /css / Javascript',key:'2'},
-        {title:'Node JS and express',rating:8,body:'express / mongoDb',key:'3'},
+        {title:'Data Structure and algorithm',rating:3,body:'dsa by java',key:'1'},
+        {title:'React.Js and redux ',rating:4,body:'Html /css / Javascript',key:'2'},
+        {title:'Node JS and express',rating:4,body:'express / mongoDb',key:'3'},
     ])
     return (
         <View style={globalStyles.container}>
             <FlatList 
             data={reviews}
             renderItem={({item})=>(
+                
                 <TouchableOpacity onPress={()=>navigation.navigate('ReviewDetails',item)}>
+                    <Card>
                     <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </Card>
                 </TouchableOpacity>
             )} />
         </View>
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import {StyleSheet,View,Text} from 'react-native'
-import {globalStyles} from '../styles/global'
+import {StyleSheet,View,Text, Image} from 'react-native'
+import Card from '../shared/card';
+import {globalStyles,images} from '../styles/global'
 
 
 export default function ReviewDetails({navigation}){
+    const rating=navigation.getParam('rating')
     return (
         <View style={globalStyles.container}>
-            <Text>{navigation.getParam('title')}</Text>
+           <Card>
+           <Text>{navigation.getParam('title')}</Text>
             <Text>{navigation.getParam('body')}</Text>
-            <Text>{navigation.getParam('rating')}</Text>
+            <View style={styles.rating}>
+                <Text> BookZone rating: </Text>
+                <Image source={require(images.ratings[rating])}/>
+            </View>
+           </Card>
         </View>
     )
 }
+
+const styles=StyleSheet.create({
+    rating:{
+        flexDirection:'row',
+        justifyContent:'center',
+        paddingTop:16,
+        marginTop:16,
+        borderTopWidth:1,
+        borderTopColor:'#eee',
+    }
+})
 
