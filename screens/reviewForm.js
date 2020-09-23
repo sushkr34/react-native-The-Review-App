85bd92018f7fd191a31438b86165b6b44fc4e55a
sushkr34 <sendmailtosushant@gmail.com>
2020-09-23 20:11:05 +0530

    validation added using Yup library

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -2,43 +2,65 @@ import { Formik } from 'formik';
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity, Modal, TextInput } from 'react-native'
 import { globalStyles } from '../styles/global'
+import * as yup from 'yup'
 
-export default function ReviewForm({addReview}){
+const reviewSchema = yup.object({
+    title: yup.string()
+        .required()
+        .min(4),
+    body: yup.string().required().min(8),
+    rating: yup.string().required().test('is-num-1-5', 'Rating must be a number 1-5', (val) => {
+        return parseInt(val) < 6 && parseInt(val) > 0;
+    })
+})
+
+export default function ReviewForm({ addReview }) {
     return (
         <View style={globalStyles.container}>
             <Formik
-            initialValues={{title:'',body:'',rating:''}}
-            onSubmit={(values,actions)=>{
-               actions.resetForm();
-                addReview(values)
-            }} 
+                initialValues={{ title: '', body: '', rating: '' }}
+                validationSchema={reviewSchema}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                    addReview(values)
+                }}
             >
-            {(formikProps)=>{
-                <View>
-                    <TextInput  
-                        multiline
-                    style={globalStyles.input}
-                     placeholder='Review title'
-                     onChangeText={formikProps.handleChange('title')}
-                     value={formikProps.values.title}
-                     />
-                      <TextInput  
-                      multiline
-                    style={globalStyles.input}
-                     placeholder='Review body'
-                     onChangeText={formikProps.handleChange('body')}
-                     value={formikProps.values.body}
-                     />
-                      <TextInput  
-                    style={globalStyles.input}
-                     placeholder='Rating (1-5)'
-                     onChangeText={formikProps.handleChange('rating')}
-                     value={formikProps.values.rating}
-                     keyboardType='numeric'
-                     />
-                     <Button title='submit' color='maroon' onPress={formikProps.handleSubmit}/>
-                </View>
-            }}
+                {(formikProps) => {
+                    <View>
+                        <TextInput
+                            multiline
+                            style={globalStyles.input}
+                            placeholder='Review title'
+                            onChangeText={formikProps.handleChange('title')}
+                            value={formikProps.values.title}
+                            onBlur={formikProps.handleBlur('title')}
+                        />
+                        <Text style={globalStyles.errorText}>{formikProps.touched.title && formikProps.errors.title}</Text>
+                        <TextInput
+                            multiline
+                            style={globalStyles.input}
+                            placeholder='Review body'
+                            onChangeText={formikProps.handleChange('body')}
+                            value={formikProps.values.body}
+                            onBlur={formikProps.handleBlur('body')}
+
+                        />
+                        <Text style={globalStyles.errorText}>{formikProps.touched.body && formikProps.errors.body}</Text>
+
+                        <TextInput
+                            style={globalStyles.input}
+                            placeholder='Rating (1-5)'
+                            onChangeText={formikProps.handleChange('rating')}
+                            value={formikProps.values.rating}
+                            keyboardType='numeric'
+                            onBlur={formikProps.handleBlur('rating')}
+
+                        />
+                        <Text style={globalStyles.errorText}>{formikProps.touched.rating && formikProps.errors.rating}</Text>
+
+                        <Button title='submit' color='maroon' onPress={formikProps.handleSubmit} />
+                    </View>
+                }}
             </Formik>
         </View>
     )
